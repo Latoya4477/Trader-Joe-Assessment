407dcf42f3287062d9d61612c186fb874bc5b030
Latoya Brown <87038766+Latoya4477@users.noreply.github.com>
2021-07-27 14:16:45 -0400

    Trader-Joe'

--- src/main/java/cd/rr/trader/Main.java
+++ src/main/java/cd/rr/trader/Main.java
@@ -1,9 +1,17 @@
 package cd.rr.trader;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // Your solution here
+        double transferFee =100;
+
+        public static void portfolio(double transferAmount) {
+            double initialBalance;
+            System.out.println("newBalance");
     }
 
 }
+
---
+++ src/main/java/cd/rr/trader/StockPortfolio.java
@@ -0,0 +1,15 @@
+package cd.rr.trader;
+
+public class StockPortfolio {
+    public class stockPortfolio {
+        String cusName;
+        double values;
+        double transferFee =10;
+        stockPortfolio (String cusName, double values, double transferFee) {
+            this.cusName = cusName;
+            this.values = values;
+            this.transferFee =transferFee;
+
+
+}
+
